a5b87c56d756ccf530782561e1c141d8a4bb5919
leavirapin <leavirap974@gmail.com>
2026-06-12 22:10:55 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,25 +1,25 @@
 <?php
-session_start();
 
-$db_name = 'mysql:host=localhost;port=8889;dbname=cinephoria;charset=utf8mb4';
-$db_user = 'root';
-$db_password = 'root';
+$host = "acela.proxy.rlwy.net";
+$port = "28901";
+$dbname = "railway";
+$user = "root";
+$password = "TON_MOT_DE_PASSE_RAILWAY";
 
-$conn = new PDO($db_name,$db_user,$db_password);
- 
-function unique_id(){
-        $chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        $charLengt = strlen($chars);
-        $randomString = '';
+try {
+    $conn = new PDO(
+        "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4",
+        $user,
+        $password
+    );
 
-        for ($i= 0; $i < 20; $i++) {
-            $randomString.=$chars[mt_rand(0, $charLengt - 1)];
-     } 
-     return $randomString;
-  } 
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch(PDOException $e){
+    die("Erreur : " . $e->getMessage());
+}
 
 ?>
-
 
 
 
