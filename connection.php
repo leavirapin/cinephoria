852f7642a23d0594b1c8770e154c9364ff778d3e
leavirapin <leavirap974@gmail.com>
2026-06-12 12:52:12 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,25 +1,38 @@
 <?php
 session_start();
 
-$db_name = 'mysql:host=localhost;port=8889;dbname=cinephoria;charset=utf8mb4';
-$db_user = 'root';
-$db_password = 'root';
+$db_name = 'mysql:host=sql202.infinityfree.com;dbname=if0_42164512_cinephoria;charset=utf8mb4';
+$db_user = 'if0_42164512';
+$db_password = 'rpJQCAHBWm9u0';
 
-$conn = new PDO($db_name,$db_user,$db_password);
- 
+try {
+
+    $conn = new PDO($db_name, $db_user, $db_password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch(PDOException $e){
+
+    die("Erreur connexion : " . $e->getMessage());
+
+}
+
 function unique_id(){
-        $chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        $charLengt = strlen($chars);
-        $randomString = '';
 
-        for ($i= 0; $i < 20; $i++) {
-            $randomString.=$chars[mt_rand(0, $charLengt - 1)];
-     } 
-     return $randomString;
-  } 
+    $chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charLengt = strlen($chars);
+    $randomString = '';
+
+    for ($i = 0; $i < 20; $i++) {
+
+        $randomString .= $chars[mt_rand(0, $charLengt - 1)];
+
+    }
+
+    return $randomString;
+
+}
 
 ?>
-
 
 
 
