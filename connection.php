4a51787ae2c4520990ed157df8db8db078a2f20d
leavirapin <leavirap974@gmail.com>
2026-06-12 23:52:24 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -7,6 +7,7 @@ $user = "root";
 $password = "kNkXlDMOibBroYoyTCFeDdsvGweAIdsT";
 
 try {
+
     $conn = new PDO(
         "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4",
         $user,
@@ -15,11 +16,13 @@ try {
 
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-} catch(PDOException $e) {
-    die("Erreur : " . $e->getMessage());
-}
-?>
+} catch (PDOException $e) {
 
+    die("Erreur : " . $e->getMessage());
+
+}
+
+?>
 
 
 
