f665650fd088efcd79bc97195742f83d77abe793
leavirapin <leavirap974@gmail.com>
2026-06-13 13:30:32 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,27 +1,20 @@
 <?php
-
 $host = "acela.proxy.rlwy.net";
-$port = "28901";
+$port = 28901;
 $dbname = "railway";
 $user = "root";
 $password = "kNkXlDMOibBroYoyTCFeDdsvGweAIdsT";
 
 try {
-
-    $conn = new PDO(
-        "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4",
-        $user,
-        $password
-    );
-
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
+    $conn = new PDO(
+        "mysql:host={$host};port={$port};dbname={$dbname};charset=utf8mb4",
+        $user,
+        $password,
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+    );
 } catch (PDOException $e) {
-
-    die("Erreur : " . $e->getMessage());
-
+    die("Erreur : " . $e->getMessage());
 }
-
 ?>
 
 
